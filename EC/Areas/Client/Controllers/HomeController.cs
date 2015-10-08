b957bea276b230e7f61f315a68c5b70f2d0591b4
MaiLT <lytieumai06@gmail.com>
2015-10-08 23:57:50 +0700

    Up 1
    tạo mới

---
+++ EC/Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EC.Areas.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Client/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
